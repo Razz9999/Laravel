a51e638aafd206f50dbe58f1d6a3ad07fb43a470
unknown <spr8198@gmail.com>
2019-04-10 22:34:44 +0530

    first commit

---
+++ home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.app')
+@section('content')
+<h1>Home</h1>
+<p>Bacon ipsum dolor amet prosciutto short loin hamburger, pancetta pork belly jowl filet mignon capicola ribeye biltong short ribs ground round. Shank filet mignon fatback ribeye, capicola short loin kielbasa strip steak. Ham pork belly short loin, pork chop chuck jowl doner biltong alcatra ball tip tongue rump short ribs bacon. Spare ribs sausage pork chop tri-tip chicken kielbasa.</p>
+@endsection
